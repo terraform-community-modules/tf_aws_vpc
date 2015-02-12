37a980dd736e66d386db71d5505e1f9a58c5f724
Casey Ransom <casey.ransom@lookout.com>
2015-02-12 14:23:45 -0500

    No need to be real verbose with output variable names

--- outputs.tf
+++ outputs.tf
@@ -1,20 +1,20 @@
-output "${mytag}_vpc_id" {
+output "vpc_id" {
     value = "${aws_vpc.vpc.id}"
 }
-output "vpc_cidr_block" {
+output "cidr_block" {
     value = "${aws_vpc.vpc.cidr_block}"
 }
-output "vpc_route_table_id" {
+output "route_table_id" {
     value = "${aws_vpc.vpc.main_route_table_id}"
 }
-output "vpc_default_network_acl_id" {
+output "default_network_acl_id" {
     value = "${aws_vpc.vpc.default_network_acl_id}"
 }
-output "vpc_default_security_group_id" {
+output "default_security_group_id" {
     value = "${aws_vpc.vpc.default_security_group_id}"
 }
 /* docs say this exist, it doesn't
-output "vpc_instance_tenancy" {
+output "instance_tenancy" {
     value = "${aws_vpc.vpc.instance_tenancy}"
 }
 */
