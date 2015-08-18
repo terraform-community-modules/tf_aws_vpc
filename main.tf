72cc569b12c184b5a5c39ed90b542d2bed059444
Jason Wieringa <jason@jasonwieringa.com>
2015-08-17 22:21:04 -0400

    public route association should use var.public_subnets

--- main.tf
+++ main.tf
@@ -46,7 +46,7 @@ resource "aws_route_table_association" "private" {
 }
 
 resource "aws_route_table_association" "public" {
-  count = "${length(split(",", var.private_subnets))}"
+  count = "${length(split(",", var.public_subnets))}"
   subnet_id = "${element(aws_subnet.public.*.id, count.index)}"
   route_table_id = "${aws_route_table.public.id}"
 }
