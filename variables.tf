81d183a68715431f63354a21de9db93afa2811b0
seph <seph@directionless.org>
2015-11-10 16:34:23 -0500

    Set variable subnet default values

--- variables.tf
+++ variables.tf
@@ -1,5 +1,5 @@
 variable "name" { }
 variable "cidr" { }
-variable "public_subnets" { }
-variable "private_subnets" { }
+variable "public_subnets" { default = "" }
+variable "private_subnets" { default = "" }
 variable "azs" { }
