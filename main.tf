ec10272d9b4b0dcbaa31932bf439d9efa907c2f9
seph <seph@directionless.org>
2015-11-10 16:30:45 -0500

    Add compact() to the counts to support skipping null strings

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_subnet" "private" {
   vpc_id = "${aws_vpc.mod.id}"
   cidr_block = "${element(split(",", var.private_subnets), count.index)}"
   availability_zone = "${element(split(",", var.azs), count.index)}"
-  count = "${length(split(",", var.private_subnets))}"
+  count = "${length(compact(split(",", var.private_subnets)))}"
   tags { Name = "${var.name}-private" }
 }
 
@@ -33,20 +33,20 @@ resource "aws_subnet" "public" {
   vpc_id = "${aws_vpc.mod.id}"
   cidr_block = "${element(split(",", var.public_subnets), count.index)}"
   availability_zone = "${element(split(",", var.azs), count.index)}"
-  count = "${length(split(",", var.public_subnets))}"
+  count = "${length(compact(split(",", var.public_subnets)))}"
   tags { Name = "${var.name}-public" }
 
   map_public_ip_on_launch = true
 }
 
 resource "aws_route_table_association" "private" {
-  count = "${length(split(",", var.private_subnets))}"
+  count = "${length(compact(split(",", var.private_subnets)))}"
   subnet_id = "${element(aws_subnet.private.*.id, count.index)}"
   route_table_id = "${aws_route_table.private.id}"
 }
 
 resource "aws_route_table_association" "public" {
-  count = "${length(split(",", var.public_subnets))}"
+  count = "${length(compact(split(",", var.public_subnets)))}"
   subnet_id = "${element(aws_subnet.public.*.id, count.index)}"
   route_table_id = "${aws_route_table.public.id}"
 }
