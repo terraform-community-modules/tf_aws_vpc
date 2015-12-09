396f2abc6df63247789e54547d7d88eaf1b1272e
Stuart Harris <stuart.harris@red-badger.com>
2015-12-09 10:36:47 +0000

    add enable_dns_support and enable_dns_hostnames to vpc

--- variables.tf
+++ variables.tf
@@ -3,3 +3,11 @@ variable "cidr" { }
 variable "public_subnets" { }
 variable "private_subnets" { }
 variable "azs" { }
+variable "enable_dns_hostnames" {
+  description = "should be true if you want to use private DNS within the VPC"
+  default = false
+}
+variable "enable_dns_support" {
+  description = "should be true if you want to use private DNS within the VPC"
+  default = false
+}
