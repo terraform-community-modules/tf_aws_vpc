d22f24346e2da1a53e36dce49677b23403fe0932
mattclegg <cleggmatt@gmail.com>
2016-08-19 12:40:03 +0200

    Formatted files (#15)

--- main.tf
+++ main.tf
@@ -1,8 +1,11 @@
 resource "aws_vpc" "mod" {
-  cidr_block = "${var.cidr}"
+  cidr_block           = "${var.cidr}"
   enable_dns_hostnames = "${var.enable_dns_hostnames}"
-  enable_dns_support = "${var.enable_dns_support}"
-  tags { Name = "${var.name}" }
+  enable_dns_support   = "${var.enable_dns_support}"
+
+  tags {
+    Name = "${var.name}"
+  }
 }
 
 resource "aws_internet_gateway" "mod" {
@@ -10,49 +13,61 @@ resource "aws_internet_gateway" "mod" {
 }
 
 resource "aws_route_table" "public" {
-  vpc_id = "${aws_vpc.mod.id}"
+  vpc_id           = "${aws_vpc.mod.id}"
   propagating_vgws = ["${var.public_propagating_vgws}"]
-  tags { Name = "${var.name}-public" }
+
+  tags {
+    Name = "${var.name}-public"
+  }
 }
 
 resource "aws_route" "public_internet_gateway" {
-    route_table_id = "${aws_route_table.public.id}"
-    destination_cidr_block = "0.0.0.0/0"
-    gateway_id = "${aws_internet_gateway.mod.id}"
+  route_table_id         = "${aws_route_table.public.id}"
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = "${aws_internet_gateway.mod.id}"
 }
 
 resource "aws_route_table" "private" {
-  vpc_id = "${aws_vpc.mod.id}"
+  vpc_id           = "${aws_vpc.mod.id}"
   propagating_vgws = ["${var.private_propagating_vgws}"]
-  tags { Name = "${var.name}-private" }
+
+  tags {
+    Name = "${var.name}-private"
+  }
 }
 
 resource "aws_subnet" "private" {
-  vpc_id = "${aws_vpc.mod.id}"
-  cidr_block = "${var.private_subnets[count.index]}"
+  vpc_id            = "${aws_vpc.mod.id}"
+  cidr_block        = "${var.private_subnets[count.index]}"
   availability_zone = "${var.azs[count.index]}"
-  count = "${length(var.private_subnets)}"
-  tags { Name = "${var.name}-private" }
+  count             = "${length(var.private_subnets)}"
+
+  tags {
+    Name = "${var.name}-private"
+  }
 }
 
 resource "aws_subnet" "public" {
-  vpc_id = "${aws_vpc.mod.id}"
-  cidr_block = "${var.public_subnets[count.index]}"
+  vpc_id            = "${aws_vpc.mod.id}"
+  cidr_block        = "${var.public_subnets[count.index]}"
   availability_zone = "${var.azs[count.index]}"
-  count = "${length(var.public_subnets)}"
-  tags { Name = "${var.name}-public" }
+  count             = "${length(var.public_subnets)}"
+
+  tags {
+    Name = "${var.name}-public"
+  }
 
   map_public_ip_on_launch = true
 }
 
 resource "aws_route_table_association" "private" {
-  count = "${length(var.private_subnets)}"
-  subnet_id = "${element(aws_subnet.private.*.id, count.index)}"
+  count          = "${length(var.private_subnets)}"
+  subnet_id      = "${element(aws_subnet.private.*.id, count.index)}"
   route_table_id = "${aws_route_table.private.id}"
 }
 
 resource "aws_route_table_association" "public" {
-  count = "${length(var.public_subnets)}"
-  subnet_id = "${element(aws_subnet.public.*.id, count.index)}"
+  count          = "${length(var.public_subnets)}"
+  subnet_id      = "${element(aws_subnet.public.*.id, count.index)}"
   route_table_id = "${aws_route_table.public.id}"
 }
--- variables.tf
+++ variables.tf
@@ -1,30 +1,38 @@
-variable "name" { }
-variable "cidr" { }
+variable "name" {}
+
+variable "cidr" {}
+
 variable "public_subnets" {
   description = "A list of public subnets inside the VPC."
-  default = []
+  default     = []
 }
+
 variable "private_subnets" {
   description = "A list of private subnets inside the VPC."
-  default = []
+  default     = []
 }
+
 variable "azs" {
   description = "A list of Availability zones in the region"
-  default = []
+  default     = []
 }
+
 variable "enable_dns_hostnames" {
   description = "should be true if you want to use private DNS within the VPC"
-  default = false
+  default     = false
 }
+
 variable "enable_dns_support" {
   description = "should be true if you want to use private DNS within the VPC"
-  default = false
+  default     = false
 }
+
 variable "private_propagating_vgws" {
   description = "A list of VGWs the private route table should propagate."
-  default = []
+  default     = []
 }
+
 variable "public_propagating_vgws" {
   description = "A list of VGWs the public route table should propagate."
-  default = []
+  default     = []
 }
