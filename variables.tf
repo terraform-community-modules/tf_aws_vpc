fb1bf7c66bcf56d0e53102c396239fd94f586226
Robert M. Thomson <rmt-git@corporatism.org>
2017-06-06 13:19:55 +0200

    Support subnet-specific additional tags
    * Added var.public_subnet_tags
    * Added var.private_subnet_tags
    * Added var.database_subnet_tags
    * Added var.elasticache_subnet_tags
    * Removed Tier subnet tag
    
    This helps solve a specific issue with Kubernetes, where public &
    private subnets must be appropriately tagged for public/private ELBs to
    work.

--- variables.tf
+++ variables.tf
@@ -63,3 +63,23 @@ variable "tags" {
   description = "A map of tags to add to all resources"
   default     = {}
 }
+
+variable "public_subnet_tags" {
+  description = "Additional tags for the public subnets"
+  default     = {}
+}
+
+variable "private_subnet_tags" {
+  description = "Additional tags for the public subnets"
+  default     = {}
+}
+
+variable "database_subnet_tags" {
+  description = "Additional tags for the database subnets"
+  default     = {}
+}
+
+variable "elasticache_subnet_tags" {
+  description = "Additional tags for the elasticache subnets"
+  default     = {}
+}
