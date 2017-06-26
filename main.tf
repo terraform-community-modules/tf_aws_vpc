649cb084ba26f920c42fe283c8093c2dcc6ea3d9
Exequiel Pierotto <epierotto@ptc.com>
2017-06-26 09:58:03 +0100

    Adding tenancy option for instances launched into the VPC

--- main.tf
+++ main.tf
@@ -1,5 +1,6 @@
 resource "aws_vpc" "mod" {
   cidr_block           = "${var.cidr}"
+  instance_tenancy     = "${var.instance_tenancy}"
   enable_dns_hostnames = "${var.enable_dns_hostnames}"
   enable_dns_support   = "${var.enable_dns_support}"
   tags                 = "${merge(var.tags, map("Name", format("%s", var.name)))}"
